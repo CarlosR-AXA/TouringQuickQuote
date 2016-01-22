85b22233f13a9f17e85e7321ae28d2e8a9136049
Carlos Romero <carlos.romero.delgado@outlook.com>
2016-01-22 17:41:07 +0100

    Added three new steps (Page 2, 3 and 4).

--- src/test/java/es/crd/tests/touring_test/Constants.java
+++ src/test/java/es/crd/tests/touring_test/Constants.java
@@ -4,6 +4,9 @@ public class Constants {
 
 	// Expected data
 	static final String STEP_1_EXPECTED_AGE = "25";
+	static final String STEP_2_EXPECTED_BM = "0";
+	static final String STEP_3_EXPECTED_POSTAL_CODE = "1000";
+	static final String STEP_4_EXPECTED_BRAND = "TACK";
 
 	// General strings
 	static final String TOURING_ULR = "https://www.touring-assurances.be/auto/quick-quote/";
@@ -12,4 +15,17 @@ public class Constants {
 	// Age test strings
 	static final String NAME_AGE = "age";
 	static final String XPATH_AGE = "//div[@data-target='bm']";
+
+	// BM test strings
+	static final String XPATH_BUTTON = "//button[@value='0' and @class='first']";
+	static final String XPATH_BM = "//div[@data-target='postalCode']";
+
+	// Postal code test strings
+	static final String NAME_POSTAL_CODE = "postalCode";
+	static final String XPATH_POSTAL_CODE = "//div[@data-target='brand']";
+
+	// Brand test strings
+	static final String CLASS_ALL_BRANDS = "allbrands";
+	static final String CLASS_TACK = "TACK";
+	static final String XPATH_BRAND = "//div[@data-target='priceVat']";
 }
--- src/test/java/es/crd/tests/touring_test/SeleniumUnitTest.java
+++ src/test/java/es/crd/tests/touring_test/SeleniumUnitTest.java
@@ -8,6 +8,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebElement;
@@ -59,6 +60,57 @@ public class SeleniumUnitTest {
 
 		elapsedTime = (System.currentTimeMillis() - timer);
 		LOGGER.info("=== Age test took {} ms to complete ===", elapsedTime);
+
+		// ========== Step 2: BM Test==========
+		timer = System.currentTimeMillis();
+
+		element = waitAndGetResultElement(By.xpath(Constants.XPATH_BUTTON));
+		while (element.isDisplayed()) {
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+		}
+		element = waitAndGetResultElement(By.xpath(Constants.XPATH_BM + Constants.XPATH_GENERIC));
+		LOGGER.info("The expected result is \"{}\" and we got \"{}\".", Constants.STEP_2_EXPECTED_BM,
+				element.getText());
+		assertEquals(Constants.STEP_2_EXPECTED_BM, element.getText());
+
+		elapsedTime = (System.currentTimeMillis() - timer);
+		LOGGER.info("=== BM test took {} ms to complete ===", elapsedTime);
+
+		// ========== Step 3: Postal code Test==========
+		timer = System.currentTimeMillis();
+
+		element = waitAndGetResultElement(By.name(Constants.NAME_POSTAL_CODE));
+		element.sendKeys(Constants.STEP_3_EXPECTED_POSTAL_CODE);
+		element.sendKeys(Keys.ENTER);
+
+		element = waitAndGetResultElement(By.xpath(Constants.XPATH_POSTAL_CODE + Constants.XPATH_GENERIC));
+		LOGGER.info("The expected result is \"{}\" and we got \"{}\".", Constants.STEP_3_EXPECTED_POSTAL_CODE,
+				element.getText());
+		assertEquals(Constants.STEP_3_EXPECTED_POSTAL_CODE, element.getText());
+
+		elapsedTime = (System.currentTimeMillis() - timer);
+		LOGGER.info("=== Postal code test took {} ms to complete ===", elapsedTime);
+
+		// ========== Step 4: Brand Test==========
+		timer = System.currentTimeMillis();
+
+		element = waitAndGetResultElement(By.className(Constants.CLASS_ALL_BRANDS));
+		while (element.isDisplayed()) {
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+		}
+
+		element = waitAndGetResultElement(By.className(Constants.CLASS_TACK));
+		while (element.isDisplayed()) {
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+		}
+
+		element = waitAndGetResultElement(By.xpath(Constants.XPATH_BRAND + Constants.XPATH_GENERIC));
+		LOGGER.info("The expected result is \"{}\" and we got \"{}\".", Constants.STEP_4_EXPECTED_BRAND,
+				element.getText());
+		assertEquals(Constants.STEP_4_EXPECTED_BRAND, element.getText());
+
+		elapsedTime = (System.currentTimeMillis() - timer);
+		LOGGER.info("=== Brand test took {} ms to complete ===", elapsedTime);
 	}
 
 	@AfterClass
